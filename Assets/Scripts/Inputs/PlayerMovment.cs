be4f11b52e7f14ef992597e6bb9f6be2c8da4858
Laurenzio103 <consolilorenzo04@gmail.com>
2022-11-09 18:06:31 +0100

    Player movment with camera

--- Assets/Scripts/Inputs/PlayerMovment.cs
+++ Assets/Scripts/Inputs/PlayerMovment.cs
@@ -10,6 +10,14 @@ public class PlayerMovment : MonoBehaviour
 
     [SerializeField] private float speed;
 
+    [SerializeField] private float minVerticalOffset = -3.064649f;
+
+    [SerializeField] private float maxVerticalOffset = 1.500422f;
+
+    [SerializeField] private float minHorizzontalOffset = -6.45f;
+
+    [SerializeField] private float maxHorizzontalffset = 6.45f;
+
     private void Awake()
     {
         _playerInputs = GetComponent<IInput>();
@@ -18,7 +26,33 @@ public class PlayerMovment : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _playerRigidbody.velocity = (transform.right * _playerInputs.MoveDirection.x + transform.up * _playerInputs.MoveDirection.y).normalized * speed;
+
+        Vector3 fixedDir = (transform.right * _playerInputs.MoveDirection.x + transform.up * _playerInputs.MoveDirection.y).normalized;
+
+        if (transform.position.x >= maxHorizzontalffset && fixedDir.x > 0.1f)
+        {
+            fixedDir.x = 0f;
+        }
+
+        if (transform.position.x <= minHorizzontalOffset && fixedDir.x < 0.1f)
+        {
+            fixedDir.x = 0f;
+        }
+
+        if (transform.position.y >= maxVerticalOffset && fixedDir.y > 0.1f)
+        {
+            fixedDir.y = 0f;
+            fixedDir.z = 0f;
+        }
+
+        if (transform.position.y <= minVerticalOffset && fixedDir.y < 0.1f)
+        {
+            fixedDir.y = 0f;
+            fixedDir.z = 0f;
+        }
+
+        _playerRigidbody.velocity = fixedDir * speed;
+        //_playerRigidbody.velocity = _playerInputs.MoveDirection * speed;
     }
 
 }
